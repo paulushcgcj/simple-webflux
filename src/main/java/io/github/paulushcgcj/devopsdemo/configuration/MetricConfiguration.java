7a77349b580c25f0cb4745a866e01013c8bc9325
Paulo Gomes da Cruz Junior <pcruz@daitan.com>
2022-09-13 13:52:47 -0700

    feat(tracing): adding tracing functionality and external config

--- src/main/java/io/github/paulushcgcj/devopsdemo/configuration/MetricConfiguration.java
+++ src/main/java/io/github/paulushcgcj/devopsdemo/configuration/MetricConfiguration.java
@@ -1,15 +1,18 @@
 package io.github.paulushcgcj.devopsdemo.configuration;
 
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
+import brave.handler.MutableSpan;
+import brave.handler.SpanHandler;
+import brave.propagation.TraceContext;
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.config.MeterFilter;
 import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MetricConfiguration {
@@ -21,21 +24,23 @@ public class MetricConfiguration {
 
   @Bean
   public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-    return registry -> registry.config()
-        .commonTags(
-            "version", "1.0.0",
-            "product", "company"
-        )
-        .meterFilter(
-            new MeterFilter() {
-              @Override
-              public DistributionStatisticConfig configure(Meter.Id id, DistributionStatisticConfig config) {
-                return DistributionStatisticConfig.builder()
-                    .percentiles(0.5,0.95,0.99)
-                    .build()
-                    .merge(config);
-              }
-            });
+    return registry -> {
+      registry.config()
+          .commonTags(
+              "version", "1.0.0",
+              "product", "company"
+          )
+          .meterFilter(
+              new MeterFilter() {
+                @Override
+                public DistributionStatisticConfig configure(Meter.Id id, DistributionStatisticConfig config) {
+                  return DistributionStatisticConfig.builder()
+                      .percentiles(0.5, 0.95, 0.99)
+                      .build()
+                      .merge(config);
+                }
+              });
+    };
   }
 
   @Bean
@@ -46,6 +51,18 @@ public class MetricConfiguration {
   @Bean
   public MeterFilter ignoreTag() {
     return MeterFilter.ignoreTags("type");
+  }
+
+
+  @Bean
+  @ConditionalOnProperty(value = "spring.zipkin.enabled", havingValue = "false")
+  public SpanHandler spanHandler() {
+    return new SpanHandler() {
+      @Override
+      public boolean end(TraceContext context, MutableSpan span, Cause cause) {
+        return true;
+      }
+    };
   }
 
 }
