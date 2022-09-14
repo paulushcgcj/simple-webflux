d2b5c3b4471865eb7746755cc33bf850654bac6b
Paulo Gomes da Cruz Junior <pcruz@daitan.com>
2022-09-14 14:28:35 -0700

    feat(db): migrating to postgres

--- src/test/java/io/github/paulushcgcj/devopsdemo/configuration/SpringTestConfig.java
+++
@@ -1,9 +0,0 @@
-package io.github.paulushcgcj.devopsdemo.configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@ComponentScan
-@Configuration
-public class SpringTestConfig {
-}
