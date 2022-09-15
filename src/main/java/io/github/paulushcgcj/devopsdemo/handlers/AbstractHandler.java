be639edf2314ffb76cb9510e4bcae9d613c87f7d
Paulo Gomes da Cruz Junior <pcruz@daitan.com>
2022-09-15 08:33:23 -0700

    feat(security): adding security with dummy user

--- src/main/java/io/github/paulushcgcj/devopsdemo/handlers/AbstractHandler.java
+++ src/main/java/io/github/paulushcgcj/devopsdemo/handlers/AbstractHandler.java
@@ -1,17 +1,31 @@
 package io.github.paulushcgcj.devopsdemo.handlers;
 
+import java.security.Principal;
 import java.util.function.Consumer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.server.ResponseStatusException;
 
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
+@Slf4j
 public abstract class AbstractHandler<T> {
   protected final Class<T> contentClass;
+
+  protected static Mono<Void> logPrincipoal(ServerRequest request){
+    return
+        request
+            .principal()
+            .map(Principal::getName)
+            .doOnNext(principalName -> log.info("Requesting with user {}",principalName))
+            .then();
+  }
 
   protected static Consumer<Throwable> handleError() {
     return throwable -> ServerResponse
