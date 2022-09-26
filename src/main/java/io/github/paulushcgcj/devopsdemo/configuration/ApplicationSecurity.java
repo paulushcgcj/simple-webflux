adae51c050b5aee3bde0abe917ab2b6afc313f22
Paulo Gomes da Cruz Junior <pcruz@daitan.com>
2022-09-26 14:43:22 -0700

    chore(security): changing auth url

--- src/main/java/io/github/paulushcgcj/devopsdemo/configuration/ApplicationSecurity.java
+++ src/main/java/io/github/paulushcgcj/devopsdemo/configuration/ApplicationSecurity.java
@@ -1,18 +1,16 @@
 package io.github.paulushcgcj.devopsdemo.configuration;
 
+import io.github.paulushcgcj.devopsdemo.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import io.github.paulushcgcj.devopsdemo.repositories.UserRepository;
-import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -30,18 +28,8 @@ public class ApplicationSecurity {
     http
         .authorizeExchange()
 
-        .pathMatchers(HttpMethod.GET, "/traces").permitAll()
-        .pathMatchers(HttpMethod.POST, "/traces").permitAll()
-        .pathMatchers(HttpMethod.GET, "/info").permitAll()
-        .pathMatchers(HttpMethod.GET, "/health").permitAll()
-        .pathMatchers(HttpMethod.GET, "/health/**").permitAll()
-        .pathMatchers(HttpMethod.GET, "/metrics").permitAll()
-        .pathMatchers(HttpMethod.GET, "/metrics/**").permitAll()
-        .pathMatchers(HttpMethod.GET, "/swagger**").permitAll()
-        .pathMatchers(HttpMethod.GET, "/webjars/**").permitAll()
-        .pathMatchers(HttpMethod.GET, "/v3/api-docs/**").permitAll()
-
-        .anyExchange().authenticated()
+        .anyExchange().permitAll()
+        .pathMatchers("/api/**").authenticated()
 
         .and()
 
@@ -61,7 +49,7 @@ public class ApplicationSecurity {
   @ConditionalOnMissingBean
   public ReactiveJwtDecoder jwtDecoder(
       @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") String jwkSetUri
-  ){
+  ) {
     return new NimbusReactiveJwtDecoder(jwkSetUri);
   }
 
