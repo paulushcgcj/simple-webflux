33b53b1c2f2e7163ba1ddd24ee8fb00a8bd6db74
Paulo Gomes da Cruz Junior <pcruz@daitan.com>
2022-11-04 09:25:59 -0700

    chore(security): updating matchers and adding RBAC from keycloak

--- src/main/java/io/github/paulushcgcj/devopsdemo/configuration/ApplicationSecurity.java
+++ src/main/java/io/github/paulushcgcj/devopsdemo/configuration/ApplicationSecurity.java
@@ -1,23 +1,34 @@
 package io.github.paulushcgcj.devopsdemo.configuration;
 
-import io.github.paulushcgcj.devopsdemo.repositories.UserRepository;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import com.nimbusds.jose.shaded.json.JSONArray;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
 @RequiredArgsConstructor
 public class ApplicationSecurity {
-
-  private final UserRepository userRepository;
 
   @Bean
   public SecurityWebFilterChain filterChain(
@@ -28,8 +39,8 @@ public class ApplicationSecurity {
     http
         .authorizeExchange()
 
-        .anyExchange().permitAll()
         .pathMatchers("/api/**").authenticated()
+        .anyExchange().permitAll()
 
         .and()
 
@@ -39,10 +50,21 @@ public class ApplicationSecurity {
         .cors().disable()
 
         .oauth2ResourceServer()
-        .jwt()
-        .jwtDecoder(jwtDecoder);
+        .jwt(jwt -> jwt.jwtAuthenticationConverter(converter()).jwtDecoder(jwtDecoder));
 
     return http.build();
+  }
+
+  private Converter<Jwt, Mono<AbstractAuthenticationToken>> converter() {
+    return jwt -> Mono.just(new JwtAuthenticationToken(jwt,getGrantedAuthorities(jwt)));
+  }
+
+  private static List<GrantedAuthority> getGrantedAuthorities(Jwt jwt) {
+    final JSONArray realmAccess = (JSONArray) ((Map<String,Object>)jwt.getClaims().get("realm_access")).get("roles");
+    return realmAccess.stream()
+        .map(roleName -> "ROLE_" + roleName) // prefix to map to a Spring Security "role"
+        .map(SimpleGrantedAuthority::new)
+        .collect(Collectors.toList());
   }
 
   @Bean
